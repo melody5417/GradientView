57d40a27b3331ee0edefe30dfcb68c51c9b167d0
yiqiwang <yiqiwang@tencent.com>
2017-04-11 11:40:43 +0800

    Initial Commit

---
+++ GradientView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GradientView
+//
+//  Created by yiqiwang(王一棋) on 2017/4/11.
+//  Copyright © 2017年 melody5417. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
